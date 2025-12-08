1de2db65d60a1b41c73978618480b5c1b075db3e
alaa3663 <alaaaad77@gmail.com>
2025-12-08 02:09:53 +0100

    Refactor counter.js to include Bob's visibility logic

--- counter.js
+++ counter.js
@@ -5,6 +5,7 @@ age=document.getElementById("age");
 message=document.getElementById("message");
 let count =0;
 decrease.onclick=function(){
+    document.getElementById('Bob').style.visibility = 'hidden';
     if(count>0){
         count--;
         age.textContent=count;
@@ -15,13 +16,22 @@ decrease.onclick=function(){
     }
 
 increase.onclick=function(){
+    document.getElementById('Bob').style.visibility = 'hidden';
     count++;
     age.textContent=count;
     message.textContent="";
 }
 confirm.onclick=function(){
-    message.textContent="Sike , keep your age for yourself , today Bob is going to guess your age!";
+    let realAge = age.textContent;
+    message.textContent="Sike , keep your age for yourself , today SpongeBob is going to guess your age!";
+    document.getElementById('Bob').style.visibility = 'visible';
+    document.getElementById('phrasestyle').style.visibility = 'visible';
+    
 }
+document.getElementById('Bob').onclick=function(){
+    document.getElementById('s1').style.visibility = 'visible';
+}
+
 
 
 
